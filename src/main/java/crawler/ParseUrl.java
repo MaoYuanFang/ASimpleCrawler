76fdc47b9780b12d64890664247f8ad2aae1cba3
Maoyuan <295608936@qq.com>
2019-10-02 08:39:49 +0800

    待解决异步资源

--- src/main/java/crawler/ParseUrl.java
+++ src/main/java/crawler/ParseUrl.java
@@ -1,6 +1,5 @@
 package crawler;
 
-import com.sun.jmx.remote.internal.ArrayQueue;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -14,6 +13,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class ParseUrl {
@@ -21,19 +21,28 @@ public class ParseUrl {
 
 
     public static void main(String[] args) throws IOException {
-        String url = "https://sina.cn";
+        String url = "https://news.sina.cn/global/szzx/2019-09-29/detail-iicezzrq9104996.d.html";
         HashSet<String> set = new HashSet<>();
         set.add(url);
-        sovle(url ,set);
+        Queue<String> queue = new LinkedList<>();
+        queue.add(url);
+        init(set,queue);
 
     }
 
-    private static boolean 判断条件(String url){
-        return url.matches("http(.*).sina.cn/(.*)") ;
+    private static boolean filter(String url){
+        return url.matches("http(.*).sina.cn/(.*)-(.*)-(.*)") && !url.matches("(.*)passport(.*)") &&
+                !url.matches("(.*)gaokao(.*)") && !url.matches("(.*)games(.*)");
     }
 
-    private static void sovle(String url , HashSet<String> filter) throws IOException {
+
+
+    private static void init(HashSet<String> linkList, Queue<String> queue) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
+        if (queue.isEmpty()){
+
+        }
+        String url = queue.remove();
         HttpGet get = new HttpGet(url);
         get.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36");
         try (CloseableHttpResponse response = httpclient.execute(get)) {
@@ -50,14 +59,14 @@ public class ParseUrl {
             Elements links = document.select("a");
             for (Element element : links) {
                 String href = element.attr("href");
-                System.out.println(href);
-                if (判断条件(href) && !filter.contains(href)) {
-                    filter.add(href);
+                linkList.add(href);
+                if (filter(href) ) {
+                    queue.add(href);
                     //添加到库
-                    sovle(href, filter);
                 }
             }
         }
+        init(linkList,queue);
         System.out.println("到底了");
     }
 }
